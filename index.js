542ce87a2e97368b9905734954174a48a8f3d56f
FatimaEzzahra IGH <fatimaezzahraighil7@gmail.com>
2026-04-22 17:46:06 +0100

    Ajout de la fonction soustraction

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ if (require.main === module) {
 }
 
 module.exports = { addition };
+
+
+function addition(a, b) { 
+return a + b; 
+} 
+function soustraction(a, b) { 
+return a - b; 
+}
+if (require.main === module) { 
+console.log("Résultat addition:", addition(5, 3)); 
+console.log("Résultat soustraction:", soustraction(5, 3)); 
+} 
+module.exports = { addition, soustraction }; 
